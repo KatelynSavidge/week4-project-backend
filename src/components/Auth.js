c3a87e42fcf85b34c0e31eda6945377f006b0d2c
Katelyn Savidge <ktsavidge@gmail.com>
2022-12-16 15:26:18 -0600

    first

---
+++ src/components/Auth.js
@@ -0,0 +1,60 @@
+import {useState, useContext } from 'react';
+import axios from 'axios';
+import AuthContext  from '../store/authContext';
+ 
+const Auth = () => {
+    
+   const [username, setUsername] = useState('')
+   const [password, setPassword] = useState('')
+   const [register, setRegister] = useState(true)
+ 
+   const submitHandler = e => {
+       e.preventDefault()
+ 
+       const body = {
+            username,
+            password
+       }
+
+    const authCtx = useContext(AuthContext)
+
+    const url = 'http://localhost:5005/'
+
+    axios.post(register ? `${url}/register` : `${url}/login`, body)
+    .then(({data}) => {
+        console.log('AFTER AUTH', data)
+    })
+    .catch(err => {
+        setPassword('')
+        setUsername('')
+    })
+
+    authCtx.login(res.data.token, res.data.exp, res.data.userId)
+ }
+
+   return (
+       <main>
+           <h1>Welcome!</h1>
+           <form className='form auth-form' onSubmit={submitHandler}>
+               <input
+                   type='text'
+                   placeholder='username'
+                   value={username}
+                   onChange={e => setUsername(e.target.value)}
+                   className='form-input'/>
+               <input
+                   type='password'
+                   placeholder='password'
+                   value={password}
+                   onChange={e => setPassword(e.target.value)}
+                   className='form-input'/>
+               <button className='form-btn'>
+                   {register ? 'Sign Up' : 'Login'}
+               </button>
+           </form>
+           <button className='form-btn'>Need to {register ? 'Login' : 'Sign Up'}?</button>
+       </main>
+   )
+}
+ 
+export default Auth
